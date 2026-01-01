af4c9696eef2364e3ceb7e2fb1319d591f168e78
ashan012 <khan89891918@outlook.com>
2026-01-01 21:39:00 +0500

    add watchHistory api

---
+++ src/app/api/(user)/user-watch-history/route.js
@@ -0,0 +1,58 @@
+import { dbconnect } from "@/lib/dbconnect";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option";
+import UserModel from "@/models/users.model";
+import { ApiError } from "@/utils/ApiError";
+import { NextResponse } from "next/server";
+import { ApiResponse } from "@/utils/ApiResponse";
+//import VideoModel from "@/models/vidoes.model";
+
+export async function GET() {
+  await dbconnect();
+  try {
+    const session = await getServerSession(authOptions);
+    const userId = session._id;
+
+    if (!userId) {
+      throw new ApiError("user not found", 404);
+    }
+    const watchHistory = await UserModel.findOne({ _id: userId })
+      .select({ watchHistory: { $slice: -10 } })
+      .select(
+        "-password -email -fullName -coverImage -updateAt -createdAt -username -avatar -updatedAt"
+      )
+      .populate({
+        path: "watchHistory",
+        select: "title description thumbnail ",
+      })
+      .lean();
+
+    console.log(watchHistory);
+
+    if (!watchHistory) {
+      throw new ApiError("failed to watchHistory", 500);
+    }
+
+    return NextResponse.json(
+      new ApiResponse(
+        true,
+        "fetch watchHistory successfully",
+        200,
+        watchHistory
+      )
+    );
+  } catch (error) {
+    console.error(error?.message || error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: error?.message || "error on watch History fetching",
+        status: error?.status || 400,
+      },
+      {
+        status: error?.status || 400,
+      }
+    );
+  }
+}
